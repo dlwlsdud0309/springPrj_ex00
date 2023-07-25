ce2255f0f22761384e65862fca1377628374e115
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-07-25 16:13:29 +0900

    유튜브 강의 참조하여 HikariCP와 mybatis, mybatis-spring 버전 수정, HikariPool-1과 실행 시 바로 종료되는 로그 계속 확인됨

--- src/test/java/org/zerock/persistence/DataSourceTests.java
+++ src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -17,6 +17,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
+// ex00
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
